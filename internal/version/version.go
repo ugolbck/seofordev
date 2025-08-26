b73dfaa2280e9887e0de12db953e6c83ffb024f1
Ugo <ugo813@hotmail.fr>
2025-08-26 21:06:12 +0200

    bumped required version to 1.0.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -10,7 +10,7 @@ import (
 var Version = "dev"
 
 // MinimumRequiredVersion is the minimum version required to run the app
-const MinimumRequiredVersion = "0.3.0"
+const MinimumRequiredVersion = "1.0.0"
 
 // GetVersion returns the current version
 func GetVersion() string {
@@ -23,12 +23,12 @@ func GetVersion() string {
 // IsVersionAtLeast checks if the current version meets the minimum requirement
 func IsVersionAtLeast(minVersion string) (bool, error) {
 	currentVersion := GetVersion()
-	
+
 	// Development builds always pass version check
 	if currentVersion == "dev" {
 		return true, nil
 	}
-	
+
 	return compareVersions(currentVersion, minVersion)
 }
 
@@ -38,11 +38,11 @@ func CheckMinimumVersion() error {
 	if err != nil {
 		return fmt.Errorf("failed to validate version: %w", err)
 	}
-	
+
 	if !isValid {
 		return fmt.Errorf("version too old: current version %s, minimum required version %s", GetVersion(), MinimumRequiredVersion)
 	}
-	
+
 	return nil
 }
 
@@ -52,11 +52,11 @@ func compareVersions(version1, version2 string) (bool, error) {
 	// Remove 'v' prefix if present
 	v1 := strings.TrimPrefix(version1, "v")
 	v2 := strings.TrimPrefix(version2, "v")
-	
+
 	// Split versions into parts
 	parts1 := strings.Split(v1, ".")
 	parts2 := strings.Split(v2, ".")
-	
+
 	// Ensure we have at least 3 parts for both versions
 	for len(parts1) < 3 {
 		parts1 = append(parts1, "0")
@@ -64,19 +64,19 @@ func compareVersions(version1, version2 string) (bool, error) {
 	for len(parts2) < 3 {
 		parts2 = append(parts2, "0")
 	}
-	
+
 	// Compare each part
 	for i := 0; i < 3; i++ {
 		num1, err := strconv.Atoi(parts1[i])
 		if err != nil {
 			return false, fmt.Errorf("invalid version format: %s", version1)
 		}
-		
+
 		num2, err := strconv.Atoi(parts2[i])
 		if err != nil {
 			return false, fmt.Errorf("invalid version format: %s", version2)
 		}
-		
+
 		if num1 > num2 {
 			return true, nil
 		} else if num1 < num2 {
@@ -84,7 +84,7 @@ func compareVersions(version1, version2 string) (bool, error) {
 		}
 		// If equal, continue to next part
 	}
-	
+
 	// All parts are equal
 	return true, nil
 }
